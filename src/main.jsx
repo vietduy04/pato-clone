3afd7fd0ea0ce30799f1a1df81374dea6f377873
Viet Duy <vietduy04@gmail.com>
2026-04-21 17:06:10 +0700

    Add Posthog tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import posthog from 'posthog-js'
 import './index.css'
 import App from './App.jsx'
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+  capture_pageview: 'history_change',
+  capture_pageleave: true,
+  autocapture: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
